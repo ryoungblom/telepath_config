5e1cb8688c5b0d0d3b496368ea9510a92089b0f7
ryoungblom <rjyoungblom@gmail.com>
2022-03-29 20:53:13 -0700

    Add files via upload

---
+++ telepath_config.js
@@ -0,0 +1,148 @@
+
+/****************************************
+*****************************************
+****  Client Onboarding Landing Page ****
+*****************************************
+****************************************/
+
+var clientID = "09w475nv9y4t89843yp903q4nv093"
+
+var clientName = "Customer Name"
+
+var clientPhoneNumber = 16504794773
+
+var clientAddress = "123 Main Street"
+
+var clientCity = "San Francisco"
+
+var clientState = "CA"
+
+var clientAbout = "We are a company that helps you do medical things. We were founded by an Alien, and now we make profits."
+
+var practiceDescription = "This is a medical practice"
+
+var practiceSlogan = "We do that, too"
+
+var consultFee = 15000
+
+var consultFeePage = ("$" + str(consultFee/100));
+
+var merchantID = 1234567890
+
+var textColor = 0xffffff
+
+var primarySolidColor = 0x000000
+
+var secondarySolidColor = 0x4ac8bb
+
+var accentColor = 0x23e7b1
+
+var pageBackgroundCOlor = 0xf8c6f3a
+
+var clientLogo = "./files/logo.jpeg"
+
+var heroImage = "./files/hero.jpeg"
+
+var intakeQuestions =
+{
+
+  "Question One: " : "Answer One",
+  "Question Two: " : "Answer Two",
+  "Question Three: " : "Answer Three",
+  "Question Four: " : "Answer Four",
+  "Question Five: " : "Answer Five",
+  "Question Six: " : "Answer Six"
+}
+
+ var productOfferings =
+ {
+
+   "Drug One" : 1,
+   "Drug Two" : 2,
+   "Drug Three" : 3,
+   "Drug Four" : 4,
+   "Drug Five" : 5
+ }
+
+ var productPricing =
+ {
+   2 : .45,
+   3 : .19,
+   4 : .43,
+   1 : .89,
+   1 : .13
+ }
+
+
+var productQuestions =
+{
+  1 : "Drug Question 1",
+  2 : "Drug Question 2",
+  3 : "Drug Question 3",
+  1 : "Drug Question 4",
+  3 : "Drug Question 5"
+}
+
+
+var clientMenu =
+{
+
+  "Menu Option 1" : 1,
+  "Menu Option 2" : 2,
+  "DMenu Option 3" : 3,
+  "Menu Option 4" : 4,
+  "Menu Option 5" : 5
+}
+
+
+var menuState =
+{
+
+  1 : True,
+  2 : True,
+  3 : False,
+  4 : False,
+  5 : False
+}
+
+
+var clientDropdownLabels =
+{
+  1 : "Menu 1",
+  1 : "Menu 2",
+  1 : "Menu 3",
+  1 : "Menu 4",
+  2 : "Menu 5",
+  2 : "Menu 6",
+  2 : "Menu 7"
+}
+
+var clientDropdownLinks =
+{
+  1 : "www.google.com",
+  1 : "www.facebook.com",
+  1 : "www.amazon.com",
+  1 : "www.netflix.com",
+  2 : "www.apple.com",
+  2 : "www.ford.com",
+  2 : "www.yahoo.com"
+}
+
+var clientMenuLinks =
+{
+  3 : "www.telepath.health",
+  4 : "www.medium.com",
+  5 : "www.jctelemedicine.com",
+}
+
+
+/****************************************
+*****************************************
+*********    Client Dashboard   *********
+*****************************************
+****************************************/
+
+
+var pharmacyName = "JC Pharmacy"
+
+var pharmacyAPIendpoint = "pharmacyAPI.com/order/newOrder"
